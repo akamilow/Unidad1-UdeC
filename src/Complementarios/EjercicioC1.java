0634dc2b5f41a8d6ad29e5cea40bbe743edbd3dc
Camilo <camilocastelar3@gmail.com>
2023-09-19 16:37:28 -0500

    Actividad 1 Unidad 1 Realizado

--- src/Complementarios/EjercicioC1.java
+++ src/Complementarios/EjercicioC1.java
@@ -8,14 +8,19 @@ Formula:
 import java.util.Scanner;
 
 public class EjercicioC1 {
+    static Scanner sc = new Scanner(System.in);
+    static int lado, perimetro;
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void calcularPerimetro() {
         System.out.println("Ingrese longitud de un lado");
-        int lado = sc.nextInt();
-        int perimetro = 6 * lado;
+        lado = sc.nextInt();
+        perimetro = 6 * lado;
         System.out.println("Es perimetor del poligono es: " + perimetro);
         sc.close();
+    }
+
+    public static void main(String[] args) {
+        calcularPerimetro();
     }
     
 }
